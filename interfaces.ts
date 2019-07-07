493b32f8d0283fa6f79801ab7834ec0731af7841
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2019-07-07 21:35:20 +0200

    Intiial commit

---
+++ interfaces.ts
@@ -0,0 +1,41 @@
+export interface Request {
+  reqId?: number;
+  type: MessageType;
+  args: any;
+}
+
+export interface Response {
+  resId?: number;
+  res: any;
+}
+export enum Permission {
+  READ = "r",
+  WRITE = "w",
+  READ_WRITE = "rw"
+}
+
+export enum MessageType {
+  Hello,
+  GetValue,
+  GetDevices,
+  Notification,
+  DeviceConnected,
+  DeviceDisconnected
+}
+
+export interface DeviceConfig {
+  name: String;
+  id: String;
+  vars: Vars;
+}
+
+export interface Vars {
+  [variable: string]: Var;
+}
+
+export interface Var {
+  name: String;
+  schema: any;
+  access: Permission;
+  value: any;
+}
